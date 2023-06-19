5d51b9915840b105a2aefa5b69efb9caacabdf07
HimanshuDeveloperr <himanshugupta241299@gmail.com>
2023-06-20 02:58:41 +0530

    HomeBanner

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
 import Header from './Components/Header';
+import HomeBanner from './Components/HomeBanner';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <HomeBanner/>
     </div>
   );
 }
